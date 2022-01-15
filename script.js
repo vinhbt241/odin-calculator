77a3e1afcbe0c42a452bd028aef588a6e2d095b2
Bui The Vinh <vinhbuith32412000@gmail.com>
2022-01-15 21:20:23 +0700

    Completed all operation except +/-, bugs will be fixed later

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const equation = {
     secondNum: null
 }
 
-let displayValue = "";
+let displayValue = "0";
+const value = document.querySelector("#value");
+let displayResult = "";
+const result = document.querySelector("#result");
 
 function initButtons() {
     const buttonLayout = [
@@ -34,11 +37,6 @@ function initButtons() {
     }
 }
 
-function displayContent(content) {
-    const value = document.querySelector("#value");
-    value.textContent = content;
-}
-
 function operate(operator, num1, num2) {
     switch(operator) {
         case "+":
@@ -49,44 +47,84 @@ function operate(operator, num1, num2) {
             return num1 * num2;
         case "/":
             return num1 / num2;
+        case "%":
+            return num1 % num2;
         default:
             return;
     }
 }
 
-
 const buttons = document.querySelectorAll("#button-container button");
+
 buttons.forEach(button => {
     button.addEventListener("click", (e) => {
         let content = e.target.textContent;
+        let isNumber = !isNaN(Number(content));
+        let isDot = (content === ".")
 
-        if (isNaN(Number(content)) === false) {
-                
-            if(equation.operator === null && equation.firstNum !== null) {
-                equation.operator = displayValue;
-                displayValue = "";
+        if (content === "AC") {
+            displayValue = "0";
+            displayResult = "";
+            equation.operator = null;
+            equation.firstNum = null;
+            equation.secondNum = null;
+            value.textContent = displayValue;
+            result.textContent = displayResult;
+            return;
+        }
+
+        if (isNumber) {
+            if(equation.firstNum !== null && equation.operator !== null && equation.secondNum === null) {
+                displayValue = "0";
+                equation.secondNum = 0;
             }
-            displayValue += content;
-        } else if (content === "."){
+            if (displayValue !== "0") {
+                displayValue += content;
+            } else {
+                displayValue = content;
+            }
+        } else if (isDot) {
             if (displayValue.includes(content) === false) {
                 displayValue += content;
             }
         } else {
-            if (equation.operator !== null
-                && equation.firstNum !== null
-                && equation.secondNum !== null){
-                equation.firstNum = operate(equation.operator, equation.firstNum, equation.secondNum);
-                equation.operator = null;
-                equation.secondNum = null;
-            }
-            else if (equation.firstNum === null) {
-                equation.firstNum = Number(displayValue);
+            if (content === "C") {
+                displayValue = displayValue.toString().slice(0, -1);
+                if (displayValue === "") {
+                    displayValue = "0";
+                }
+                value.textContent = displayValue;
+                return
+            } else if (equation.firstNum !== null
+                && equation.operator !== null
+                && equation.secondNum !== null) {
+                    equation.secondNum = Math.round(Number(displayValue) * 10e6) / 10e6;
+                    if (equation.secondNum === 0 && equation.operator === "/") {
+                        alert("You can't divide a number with 0!");
+                        return;
+                    }
+                    if (content === "=") {
+                        displayValue = Math.round(operate(equation.operator, equation.firstNum, equation.secondNum) * 10e6) / 10e6;
+                        displayResult = `${equation.firstNum} ${equation.operator} ${equation.secondNum} =`;
+                        equation.firstNum = Math.round(operate(equation.operator, equation.firstNum, equation.secondNum) * 10e6) / 10e6;
+                        equation.operator = null;
+                        equation.secondNum = null;
+                    } else {
+                        equation.firstNum = Math.round(operate(equation.operator, equation.firstNum, equation.secondNum) * 10e6) / 10e6;
+                        equation.operator = content;
+                        equation.secondNum = null;
+                        displayValue = equation.firstNum;
+                        displayResult = `${equation.firstNum} ${equation.operator}`;
+                    }
             } else {
-                equation.secondNum = Number(displayValue);
+                if (equation.firstNum === null) {
+                    equation.firstNum = Math.round(Number(displayValue) * 10e6) / 10e6;
+                }
+                equation.operator = content;
+                displayResult = `${equation.firstNum} ${equation.operator}`;
             }
-            displayValue = content;
         }
-        displayContent(displayValue);
-    }) 
+        value.textContent = displayValue;
+        result.textContent = displayResult;
+    });
 })
-
